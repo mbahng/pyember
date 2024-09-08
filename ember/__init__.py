05da8b43e41cbb75f706336eaf1f1bd1d7a98152
Muchang Bahng <bahngmc@gmail.com>
2024-09-08 15:53:45 -0400

    added objectives, optimizers, samplers submodules

--- ember/__init__.py
+++ ember/__init__.py
@@ -1,11 +1,13 @@
-from .tensor_cpp import Tensor, uniform, gaussian
+from .tensor_cpp import Tensor
 from .datasets import Dataset, generate_toy_dataset
 from .models import Model, Regression, LinearRegression
+from .objectives import MSELoss
+from .optimizers import *
+from .samplers import Sampler, SGLDSampler
+from .random import gaussian, uniform
 
 __all__ = [
     "Tensor", 
-    "uniform", 
-    "gaussian", 
     "Dataset", 
     "generate_toy_dataset", 
     "Model", 
