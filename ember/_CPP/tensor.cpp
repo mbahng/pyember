fcb823a6069a45394157ed8acca0ebb60a782c5f
Muchang Bahng <bahngmc@gmail.com>
2024-10-24 14:33:10 -0400

    wrapped gradients in new GradTensor class and moved gaussian/uniform initializers as Tensor class methods

--- ember/_CPP/tensor.cpp
+++ ember/_CPP/tensor.cpp
@@ -12,7 +12,6 @@ int shape_to_length(std::vector<int> shape) {
   return len; 
 }
 
-
 std::vector<std::vector<double>> zero_matrix(int n, int m) {
   std::vector<std::vector<double>> out(n, std::vector<double>(m, 0.0)); 
   return out; 
@@ -30,57 +29,3 @@ std::vector<std::vector<double>> eye_matrix(int n, double k) {
   return out; 
 }
 
-
-/* static std::atomic<unsigned long long> seed_counter{0}; */
-/*  */
-/* Tensor gaussian(std::vector<int> shape, double mean, double stddev) { */
-/*   // Create a unique seed by combining high-resolution time and a counter */
-/*   auto now = std::chrono::high_resolution_clock::now(); */
-/*   auto nanos = std::chrono::duration_cast<std::chrono::nanoseconds>(now.time_since_epoch()).count(); */
-/*   unsigned long long unique_seed = nanos ^ (seed_counter.fetch_add(1, std::memory_order_relaxed) << 32); */
-/*  */
-/*   // Create a generator with the unique seed */
-/*   std::mt19937 generator(unique_seed); */
-/*  */
-/*   // Create a distribution */
-/*   std::normal_distribution<double> distribution(mean, stddev); */
-/*  */
-/*   // Calculate total number of elements */
-/*   int length = std::accumulate(shape.begin(), shape.end(), 1, std::multiplies<int>()); */
-/*  */
-/*   // Create and fill the vector */
-/*   std::vector<double> result(length); */
-/*   for (int i = 0; i < length; ++i) { */
-/*     result[i] = distribution(generator); */
-/*   } */
-/*  */
-/*   return Tensor(result, shape, std::vector<Tensor>()); */
-/* } */
-/*  */
-/* Tensor uniform(std::vector<int> shape, double min, double max) { */
-/*   // (Use the same unique seeding method as in the gaussian function) */
-/*   auto now = std::chrono::high_resolution_clock::now(); */
-/*   auto nanos = std::chrono::duration_cast<std::chrono::nanoseconds>(now.time_since_epoch()).count(); */
-/*   unsigned long long unique_seed = nanos ^ (seed_counter.fetch_add(1, std::memory_order_relaxed) << 32); */
-/*  */
-/*   std::mt19937 generator(unique_seed); */
-/*   std::uniform_real_distribution<double> distribution(min, max); */
-/*  */
-/*   int length = std::accumulate(shape.begin(), shape.end(), 1, std::multiplies<int>()); */
-/*   std::vector<double> result(length); */
-/*   for (int i = 0; i < length; ++i) { */
-/*     result[i] = distribution(generator); */
-/*   } */
-/*  */
-/*   return Tensor(result, shape, std::vector<Tensor>()); */
-/* } */
-/*  */
-/* Tensor ones(std::vector<int> shape) { */
-/*   std::vector<double> data(shape_to_length(shape), 1.0);  */
-/*   return Tensor(data, shape, std::vector<Tensor>());  */
-/* } */
-/*  */
-/* Tensor zeros(std::vector<int> shape) { */
-/*   std::vector<double> data(shape_to_length(shape), 0.0);  */
-/*   return Tensor(data, shape, std::vector<Tensor>());  */
-/* } */
