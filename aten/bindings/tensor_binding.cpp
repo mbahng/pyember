19c26ec67464798812740299e2744b462ce3162f
Muchang Bahng <bahngmc@gmail.com>
2024-11-23 18:28:48 -0500

    binded all methods so far for Tensor

--- aten/bindings/tensor_binding.cpp
+++ aten/bindings/tensor_binding.cpp
@@ -3,16 +3,40 @@
 void init_tensor_binding(py::module_ &m) {
   // Bind the Tensor class
   py::class_<Tensor, BaseTensor>(m, "Tensor")
-    // Constructor from data and shape
-    .def(py::init<std::vector<double>, std::vector<size_t>>())
+
+    // Constructors
+    .def(py::init<std::vector<double>, std::vector<size_t>>(), 
+        py::arg("data"), py::arg("shape"))
+    .def(py::init<std::vector<double>>(), 
+        py::arg("data")) 
+    .def(py::init<std::vector<std::vector<double>>>(), 
+        py::arg("data")) 
+    .def(py::init<std::vector<std::vector<std::vector<double>>>>(), 
+        py::arg("data")) 
+
+    .def_static("arange", &Tensor::arange, 
+        py::arg("start"), py::arg("stop"), py::arg("step"))
+    .def_static("linspace", &Tensor::linspace, 
+        py::arg("start"), py::arg("stop"), py::arg("numsteps"))
+    .def_static("gaussian", &Tensor::gaussian, 
+        py::arg("shape"), py::arg("mean"), py::arg("stddev"))
+    .def_static("uniform", &Tensor::uniform, 
+        py::arg("shape"), py::arg("min"), py::arg("max"))
+    .def_static("ones", &Tensor::ones, 
+        py::arg("shape"))
+    .def_static("zeros", &Tensor::zeros, 
+        py::arg("shape"))
+
     // Expose grad attribute
     .def_property("grad",
       [](const Tensor &t) -> const GradTensor& { return t.grad; },
       [](Tensor &t, const GradTensor &g) { t.grad = g; })
+
     // Expose prev vector
     .def_property("prev",
       [](const Tensor &t) -> const std::vector<Tensor*>& { return t.prev; },
-      [](Tensor &t, const std::vector<Tensor*> &p) { t.prev = p; })
+      [](Tensor &t, const std::vector<Tensor*> &p) { t.prev = p; }) 
+
     // String representation
     .def("__repr__",
       [](const Tensor &t) {
@@ -23,7 +47,54 @@ void init_tensor_binding(py::module_ &m) {
         }
         shape_str += ")";
         return "Tensor(shape=" + shape_str + ")";
-      });
-}
+      })
+
+    .def("__add__", 
+        [](Tensor &a, Tensor &b) {
+            return a.add(b);
+        }
+      )
+
+    .def("__add__", 
+        [](Tensor &a, GradTensor &b) {
+            return a.add(b);
+        }
+      )
+
+    .def("__sub__", 
+        [](Tensor &a, Tensor &b) {
+            return a.sub(b);
+        }
+      )
+
+    .def("__sub__", 
+        [](Tensor &a, GradTensor &b) {
+            return a.sub(b);
+        }
+      )
+
+    .def("__mul__", 
+        [](Tensor &a, Tensor &b) {
+            return a.mul(b);
+        }
+      )
+  
+    .def("__mul__", 
+        [](Tensor &a, GradTensor &b) {
+            return a.mul(b);
+        }
+      )
+  
+    .def("matmul", 
+       [](Tensor &a, Tensor &b) {
+         return a.matmul(b); 
+       })
+
+    .def("__matmul__", 
+       [](Tensor &a, Tensor &b) {
+         return a.matmul(b); 
+       })
+
+;}
 
 
