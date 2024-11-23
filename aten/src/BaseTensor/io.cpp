b6075aaa2d5ff81982f7e6ee81756bca2303d06c
Muchang Bahng <bahngmc@gmail.com>
2024-11-23 02:02:21 -0500

    added slicing support
    formatted tabwidth

--- aten/src/BaseTensor/io.cpp
+++ aten/src/BaseTensor/io.cpp
@@ -12,61 +12,56 @@ BaseTensor::operator std::string() const {
   oss << std::fixed << std::setprecision(3); 
   oss << this->type() << "\n";
 
-  // Add special case for scalar/1D results
-  if (shape_.size() <= 1 || (shape_.size() == 2 && shape_[0] == 1 && shape_[1] == 1)) {
-    oss << "[" << storage_[0] << "]\n";
-    oss << "shape = (";
-    if (shape_.empty()) {
-      oss << "1";
-    } else {
-      for (int i = 0; i < shape_.size() - 1; i++) {
-        oss << shape_[i] << ", ";
-      }
-      oss << shape_[shape_.size()-1];
+  if (shape_.size() == 1) {
+    // Special case for scalar/1D results
+    for (int i = 0; i < storage_.size(); i++) {
+      oss << std::setw(2) << std::string(2, ' '); 
+      if (storage_[i] > 0) {oss << '+' << storage_[i]; }
+      else if (storage_[i] == 0) {oss << ' ' << storage_[i]; }
+      else {oss << '-' << storage_[i]; }
     }
-    oss << "), dtype = " << this->dtype() << "\n";
-    return oss.str();
   }
+  else {
+    std::function<void(std::vector<double>, std::vector<size_t>)> print;
+    print = [&](std::vector<double> storage, std::vector<size_t> shape) {
+      if (shape.size() == 0) {
+        return;
+      }
+      else if (shape.size() == 1) {
+        for (int i = 0; i < shape[0]; i++) {
+          oss << std::setw(2) << std::string(shape_.size() - shape.size() - 1, ' '); 
 
-  std::function<void(std::vector<double>, std::vector<size_t>)> print;
-  print = [&](std::vector<double> storage, std::vector<size_t> shape) {
-    if (shape.size() == 0) {
-      return;
-    }
-    else if (shape.size() == 1) {
+          // take care of padding 
+          if (storage[i] > 0) { oss << '+' << storage[i]; } 
+          else if (storage[i] == 0) { oss << ' ' << storage[i]; } 
+          else { oss << storage[i]; }
+        }
+        return; 
+      }
+      
+      // Calculate stride for this dimension
+      size_t stride = 1;
+      for (size_t i = 1; i < shape.size(); i++) {
+        stride *= shape[i];
+      }
+      
+      // Call print recursively with remaining dimensions 
       for (int i = 0; i < shape[0]; i++) {
-        oss << std::setw(2) << std::string(shape_.size() - shape.size() - 1, ' '); 
-
-        // take care of padding 
-        if (storage[i] > 0) { oss << '+' << storage[i]; } 
-        else if (storage[i] == 0) { oss << ' ' << storage[i]; } 
-        else { oss << storage[i]; }
+        // Pass subset of storage array 
+        if (shape.size() > 2) {
+          oss << std::string(shape_.size() - shape.size(), ' ') << "[\n";
+        }
+        std::vector<double> subset(storage.begin() + i * stride, 
+                                  storage.begin() + (i + 1) * stride);
+        print(subset, std::vector<size_t>(shape.begin() + 1, shape.end())); 
+        if (shape.size() > 2) {
+          oss << std::string(shape_.size() - shape.size(), ' ') << "]";
+        }
+        oss << "\n"; 
       }
-      return; 
-    }
-    
-    // Calculate stride for this dimension
-    size_t stride = 1;
-    for (size_t i = 1; i < shape.size(); i++) {
-      stride *= shape[i];
-    }
-    
-    // Call print recursively with remaining dimensions 
-    for (int i = 0; i < shape[0]; i++) {
-      // Pass subset of storage array 
-      if (shape.size() > 2) {
-        oss << std::string(shape_.size() - shape.size(), ' ') << "[\n";
-      }
-      std::vector<double> subset(storage.begin() + i * stride, 
-                                storage.begin() + (i + 1) * stride);
-      print(subset, std::vector<size_t>(shape.begin() + 1, shape.end())); 
-      if (shape.size() > 2) {
-        oss << std::string(shape_.size() - shape.size(), ' ') << "]";
-      }
-      oss << "\n"; 
-    }
-  };
-  print(storage_, shape_);
+    };
+    print(storage_, shape_);
+  }
   oss << "\n"; 
   oss << "shape = ("; 
   for (int i = 0; i < shape_.size() - 1; i++) {
