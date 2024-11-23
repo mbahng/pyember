4396c983479b797d0c385b63fc773b2f39270d42
Muchang Bahng <bahngmc@gmail.com>
2024-11-23 14:45:14 -0500

    added C++ tests for gradtensor utils (equality and copy)

--- aten/test/GradTensor/util_tests.cpp
+++ aten/test/GradTensor/util_tests.cpp
@@ -1,12 +1,26 @@
 #include <gtest/gtest.h>
 #include "../../src/Tensor.h"
 
-TEST(UtilsTst, Stuff1) {
+TEST(UtilsTest, Equality) {
   GradTensor t1 = GradTensor({1., 2., 3., 4.}, {2, 2}, 1); 
-  GradTensor t2 = GradTensor({1., 2., 3., 4.}, {2, 2}, 0); 
- 
-  EXPECT_EQ(t1.data(), t2.data()); 
-  EXPECT_EQ(t1.shape(), t2.shape()); 
-  EXPECT_NE(t1.pivot(), t2.pivot());
+  GradTensor t2 = GradTensor({1., 2., 3., 4.}, {2, 2}, 1); 
+  GradTensor t3 = GradTensor({1., 2., 3., 4.}, {2, 2}, 0); 
+  GradTensor t4 = GradTensor({1., 2., 3., 4., 5., 6.}, {3, 2}, 1); 
+  GradTensor t5 = GradTensor({1., 2., 3., 5.}, {2, 2}, 1); 
+
+  ASSERT_TRUE(t1 == t2);
+  ASSERT_TRUE(t1 != t3);
+  ASSERT_TRUE(t1 != t4);
+  ASSERT_TRUE(t1 != t5);
+}
+
+TEST(UtilsTest, Copy) {
+  GradTensor t1 = GradTensor({1., 2., 3., 4.}, {2, 2}, 1);
+  GradTensor t2 = t1.copy(); 
+
+  // They are copies
+  ASSERT_TRUE(t1 == t2); 
+  // But do not live in the same memory address 
+  ASSERT_TRUE(&t1 != &t2);
 }
 
