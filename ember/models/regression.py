52a870eda65bb875bcc3a0269fe3fcd20506c9cd
Muchang Bahng <bahngmc@gmail.com>
2024-11-25 13:26:07 -0500

    backprop Intermediate=False debug working

--- ember/models/regression.py
+++ ember/models/regression.py
@@ -4,20 +4,20 @@ from .. import Tensor
 
 class Regression(Model): 
 
-    def __init__(self): 
-        super().__init__()
-        self.params = {}
+  def __init__(self): 
+    super().__init__()
+    self.params = {}
 
 class LinearRegression(Regression): 
 
-    def __init__(self, input_dim: int):
-        super().__init__() 
-        self.W = ember.Tensor.gaussian([input_dim, 1], 0, 1)
-        self.b = ember.Tensor.gaussian([1, 1], 0, 1)
+  def __init__(self, input_dim: int):
+    super().__init__() 
+    self.W = ember.Tensor.gaussian([input_dim, 1], 0, 1)
+    self.b = ember.Tensor.gaussian([1, 1], 0, 1)
 
-    def forward(self, X: Tensor): 
-        z1 = X @ self.W 
-        z = z1 + self.b
-        # backprop must be in here, can't be called globally for some reason
-        z.backprop(True) # False doesn't update gradients for some reason
-        return z
+  def forward(self, X: Tensor): 
+    z1 = X @ self.W 
+    z = z1 + self.b
+    # backprop must be in here, can't be called globally for some reason
+    z.backprop(False) 
+    return z
