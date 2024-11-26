b4672a676a941706bc8dca0e6fa0aac9d22f0741
Muchang Bahng <bahngmc@gmail.com>
2024-11-26 16:24:09 -0500

    added tests for gradtensor
    added tests for gradtensor

--- test/GradTensor/constructor_test.py
+++ test/GradTensor/constructor_test.py
@@ -2,22 +2,33 @@ import unittest
 from ember import GradTensor
 
 class TestGradTensorConstructors(unittest.TestCase): 
-  def nullConstructor(self): 
+  def testNullConstructor(self): 
     g = GradTensor() 
     truth = GradTensor([], [], 0) 
     self.assertEqual(g, truth)
 
-  def fullConstructor(self): 
+  def testFullConstructor(self): 
     g = GradTensor([1, 2, 3, 4], [2, 2], 1)
     self.assertEqual(g.data(), [1, 2, 3, 4]) 
     self.assertEqual(g.shape, [2, 2])
     self.assertEqual(g.pivot, 1)
 
-  def defaultConstructor(self):
+  def testDefaultConstructor(self):
     g = GradTensor([2, 2], 1)
     self.assertEqual(g.data(), [0, 0, 0, 0])
     self.assertEqual(g.shape, [2, 2])
     self.assertEqual(g.pivot, 1)
+
+  def testEyeConstructor(self):
+    g = GradTensor.eye(2, 1)
+    self.assertEqual(g.data(), [1, 0, 0, 1])
+    self.assertEqual(g.shape, [2, 2])
+    self.assertEqual(g.pivot, 1)
+
+  def testCopyConstructor(self): 
+    g = GradTensor([2, 2], 1)
+    c = g.copy() 
+    self.assertEqual(g, c)
 
 if __name__ == "__main__": 
   unittest.main(verbosity=2)
