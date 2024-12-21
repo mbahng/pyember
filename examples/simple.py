7c5985e80334be724ac0b1907b9a4f32f17f74d2
Muchang Bahng <bahngmc@gmail.com>
2024-12-21 11:55:21 -0500

    indexing should be kept constant across batching. Changed this

--- examples/simple.py
+++ examples/simple.py
@@ -1,11 +1,38 @@
 from ember import GradTensor, Tensor
 
-A = Tensor.gaussian([2, 3, 2], 0, 1, 1, True)
-B = Tensor.gaussian([4, 3, 2], 0, 1, 1, True) 
-C = A + B
-print(C) 
-C.backprop(False) 
+def testA(): 
+  A = Tensor.gaussian([2], 0, 1, 0, True)
+  B = Tensor.gaussian([3, 2], 0, 1, 1, True) 
+  C = A + B 
 
-print(A.grad.shape, A.grad.bidx, A.grad.pidx)
-print(B.grad.shape, B.grad.bidx, B.grad.pidx)
-print(C.grad.shape, C.grad.bidx, C.grad.pidx)
+  D = Tensor.gaussian([2], 0, 1, 0, True)
+  E = D + C
+
+  E.backprop(False)
+
+
+  print(A.grad.shape)
+  print(B.grad.shape)
+  print(C.grad.shape)
+  print(D.grad.shape)
+  print(E.grad.shape)
+
+def testB(): 
+  A = Tensor.gaussian([4, 2], 0, 1, 1, True)
+  B = Tensor.gaussian([3, 2], 0, 1, 1, True) 
+  C = A + B 
+
+  D = Tensor.gaussian([2], 0, 1, 0, True)
+  E = D + C
+
+  E.backprop(False)
+
+
+  print(A.grad.shape)
+  print(B.grad.shape)
+  print(C.grad.shape)
+  print(D.grad.shape)
+  print(E.grad.shape)
+
+testA()
+testB()
