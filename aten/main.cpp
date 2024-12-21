0b5e1872b10443a7c5bf3520e224046ffdd9811b
Muchang Bahng <bahngmc@gmail.com>
2024-12-21 13:49:07 -0500

    updates on example scripts

--- aten/main.cpp
+++ aten/main.cpp
@@ -1,12 +1,13 @@
-// including src/Tensor.h is essentially a forward declaration of 
-// everything inside Tensor.h
-#include "./src/Tensor.h" // double quotes mean look in current directory
-#include <vector>
+#include "./src/Tensor.h" 
 
 int main() {
-  Tensor* A = Tensor::gaussian({3, 2}, 0, 1, 0, true);
-  Tensor* B = Tensor::gaussian({2, 3}, 0, 1, 0, true); 
-  Tensor* C = A->matmul(B); 
-  C->backprop(true); 
+  Tensor* t1 = Tensor::arange(0, 5);
+  Tensor* t2 = Tensor::arange(0, 5); 
+  Tensor* s1 = t1->add(t2);
+  Tensor* truth_sum = Tensor::arange(0, 10, 2);
+  std::cout << std::string(*t1) << "\n"; 
+  std::cout << std::string(*t2) << "\n"; 
+  std::cout << std::string(*truth_sum) << "\n"; 
+  std::cout << std::string(*s1) << "\n"; 
   return 0;
 }
