58071b32b0b7e5af3db29685c39bfca83c162dcc
Muchang Bahng <bahngmc@gmail.com>
2024-12-21 13:09:28 -0500

    added basic scripts for comparing with pytorch

--- examples/simple.py
+++ examples/simple.py
@@ -1,14 +1,11 @@
 from ember import GradTensor, Tensor
 
 def testA(): 
-  A = Tensor.gaussian([2], 0, 1, 0, True)
-  B = Tensor.gaussian([3, 2], 0, 1, 1, True) 
-  C = A + B 
+  A = Tensor.gaussian([3, 4, 2], 0, 1, 1, True)
+  B = Tensor.gaussian([2, 5], 0, 1, 0, True) 
+  C = A @ B 
 
-
-  D = Tensor.gaussian([2], 0, 1, 0, True)
-  print(C.nb_indices()) 
-  print(D.nb_indices()) 
+  D = Tensor.gaussian([4, 5], 0, 1, 0, True)
   E = C * D
 
   E.backprop(False)
@@ -36,10 +33,4 @@ def testB():
   print(D.grad.shape)
   print(E.grad.shape)
 
-
-A = Tensor.gaussian([2, 2, 3], 0, 1, 1, True)
-B = Tensor.gaussian([4, 3, 2], 0, 1, 1, True) 
-C = A @ B 
-print(C)
-
-C.backprop(True)
+testA()
