9177b2b08f9ab36bad18e5271b96ed66a831cbc0
Muchang Bahng <bahngmc@gmail.com>
2024-12-24 22:05:35 -0500

    reorganized model structure in ember

---
+++ ember/models/unsupervised/k_means.py
@@ -0,0 +1,14 @@
+from ember import Tensor
+from ember.models.unsupervised import Clustering
+from ember.models import track_access
+
+class KMeans(Clustering): 
+
+  def __init__(self):
+    super().__init__() 
+    self.set_parameters()
+
+  @track_access
+  def forward(self, X: Tensor): 
+    super().forward(X)
+
