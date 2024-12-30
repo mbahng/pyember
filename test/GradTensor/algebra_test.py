e3f11509815358309d4362b1d778b46e69fd7997
Muchang Bahng <bahngmc@gmail.com>
2024-12-30 12:28:29 -0500

    fixed initialization errors in python algebra tests

--- test/GradTensor/algebra_test.py
+++ test/GradTensor/algebra_test.py
@@ -5,12 +5,12 @@ from ember import GradTensor, Tensor
 class TestNeg(unittest.TestCase):
 
   def testNegate(self): 
-    x = GradTensor([1], [1], 0) 
-    y = GradTensor([1, 2, 3, 4], [4], 0) 
-    z = GradTensor([1, 2, 3, 4], [2, 2], 1) 
-    xt = GradTensor([-1], [1], 0) 
-    yt = GradTensor([-1, -2, -3, -4], [4], 0) 
-    zt = GradTensor([-1, -2, -3, -4], [2, 2], 1) 
+    x = GradTensor(1) 
+    y = GradTensor([1, 2, 3, 4], [4], 0, 0) 
+    z = GradTensor([1, 2, 3, 4], [2, 2], 0, 1) 
+    xt = GradTensor(-1)
+    yt = GradTensor([-1, -2, -3, -4], [4], 0, 0) 
+    zt = GradTensor([-1, -2, -3, -4], [2, 2], 0, 1) 
     self.assertEqual(-x, xt)
     self.assertEqual(-y, yt)
     self.assertEqual(-z, zt)
@@ -21,9 +21,9 @@ class TestNeg(unittest.TestCase):
 class TestGradAdd(unittest.TestCase): 
 
   def testAddGradTensor(self): 
-    x = GradTensor([1, 2, 3, 4], [2, 2], 1)
-    y = GradTensor([1, 2, 3, 4], [2, 2], 1)
-    truth = GradTensor([2, 4, 6, 8], [2, 2], 1)
+    x = GradTensor([1, 2, 3, 4], [2, 2], 0, 1)
+    y = GradTensor([1, 2, 3, 4], [2, 2], 0, 1)
+    truth = GradTensor([2, 4, 6, 8], [2, 2], 0, 1)
     self.assertEqual(x + y, truth)
     self.assertEqual(y + x, truth)
     self.assertEqual(x.__add__(y), truth)
@@ -32,7 +32,7 @@ class TestGradAdd(unittest.TestCase):
     self.assertEqual(y.__radd__(x), truth)
 
   def testAddTensor(self): 
-    x = GradTensor([1, 2, 3, 4], [2, 2], 1)
+    x = GradTensor([1, 2, 3, 4], [2, 2], 0, 1)
     y = Tensor([1, 2, 3, 4], [2, 2])
     truth = Tensor([2, 4, 6, 8], [2, 2])
     self.assertEqual(x + y, truth)
@@ -43,9 +43,9 @@ class TestGradAdd(unittest.TestCase):
     self.assertEqual(y.__radd__(x), truth)
 
   def testAddFloat(self): 
-    x = GradTensor([1, 2, 3, 4], [2, 2], 1)
+    x = GradTensor([1, 2, 3, 4], [2, 2], 0, 1)
     y = 1.
-    truth = GradTensor([2, 3, 4, 5], [2, 2], 1)
+    truth = GradTensor([2, 3, 4, 5], [2, 2], 0, 1)
     self.assertEqual(x + y, truth)
     self.assertEqual(y + x, truth) 
     self.assertEqual(x.__add__(y), truth)
@@ -54,10 +54,10 @@ class TestGradAdd(unittest.TestCase):
 class TestGradSub(unittest.TestCase): 
 
   def testSubGradTensor(self): 
-    x = GradTensor([1, 2, 3, 4], [2, 2], 1)
-    y = GradTensor([4, 5, 6, 7], [2, 2], 1)
-    truth1 = GradTensor([-3, -3, -3, -3], [2, 2], 1)
-    truth2 = GradTensor([3, 3, 3, 3], [2, 2], 1)
+    x = GradTensor([1, 2, 3, 4], [2, 2], 0, 1)
+    y = GradTensor([4, 5, 6, 7], [2, 2], 0, 1)
+    truth1 = GradTensor([-3, -3, -3, -3], [2, 2], 0, 1)
+    truth2 = GradTensor([3, 3, 3, 3], [2, 2], 0, 1)
     self.assertEqual(x - y, truth1)
     self.assertEqual(y - x, truth2)
     self.assertEqual(x.__sub__(y), truth1)
@@ -66,7 +66,7 @@ class TestGradSub(unittest.TestCase):
     self.assertEqual(y.__rsub__(x), truth1)
 
   def testSubTensor(self): 
-    x = GradTensor([1, 2, 3, 4], [2, 2], 1)
+    x = GradTensor([1, 2, 3, 4], [2, 2], 0, 1)
     y = Tensor([1, 2, 3, 4], [2, 2])
     truth = Tensor([0, 0, 0, 0], [2, 2])
     self.assertEqual(x - y, truth)
@@ -77,10 +77,10 @@ class TestGradSub(unittest.TestCase):
     self.assertEqual(y.__rsub__(x), truth)
 
   def testSubFloat(self): 
-    x = GradTensor([1, 2, 3, 4], [2, 2], 1)
+    x = GradTensor([1, 2, 3, 4], [2, 2], 0, 1)
     y = 1
-    truth1 = GradTensor([0, 1, 2, 3], [2, 2], 1)
-    truth2 = GradTensor([0, -1, -2, -3], [2, 2], 1)
+    truth1 = GradTensor([0, 1, 2, 3], [2, 2], 0, 1)
+    truth2 = GradTensor([0, -1, -2, -3], [2, 2], 0, 1)
     self.assertEqual(x - y, truth1)
     self.assertEqual(y - x, truth2)
     self.assertEqual(x.__sub__(y), truth1)
@@ -89,9 +89,9 @@ class TestGradSub(unittest.TestCase):
 class TestGradMul(unittest.TestCase): 
 
   def testMulGradTensor(self): 
-    x = GradTensor([1, 2, 3, 4], [2, 2], 1)
-    y = GradTensor([1, 2, 3, 4], [2, 2], 1)
-    truth = GradTensor([1, 4, 9, 16], [2, 2], 1)
+    x = GradTensor([1, 2, 3, 4], [2, 2], 0, 1)
+    y = GradTensor([1, 2, 3, 4], [2, 2], 0, 1)
+    truth = GradTensor([1, 4, 9, 16], [2, 2], 0, 1)
     self.assertEqual(x * y, truth)
     self.assertEqual(y * x, truth)
     self.assertEqual(x.__mul__(y), truth)
@@ -100,7 +100,7 @@ class TestGradMul(unittest.TestCase):
     self.assertEqual(y.__rmul__(x), truth)
 
   def testMulTensor(self): 
-    x = GradTensor([1, 2, 3, 4], [2, 2], 1)
+    x = GradTensor([1, 2, 3, 4], [2, 2], 0, 1)
     y = Tensor([1, 2, 3, 4], [2, 2])
     truth = Tensor([1, 4, 9, 16], [2, 2])
     self.assertEqual(x * y, truth)
@@ -111,9 +111,9 @@ class TestGradMul(unittest.TestCase):
     self.assertEqual(y.__rmul__(x), truth)
 
   def testMulFloat(self): 
-    x = GradTensor([1, 2, 3, 4], [2, 2], 1)
+    x = GradTensor([1, 2, 3, 4], [2, 2], 0, 1)
     y = 2
-    truth = GradTensor([2, 4, 6, 8], [2, 2], 1)
+    truth = GradTensor([2, 4, 6, 8], [2, 2], 0, 1)
     self.assertEqual(x * y, truth)
     self.assertEqual(y * x, truth)
     self.assertEqual(x.__mul__(y), truth)
@@ -122,9 +122,9 @@ class TestGradMul(unittest.TestCase):
 class TestGradMatmul(unittest.TestCase): 
 
   def testMatmulGradTensor(self): 
-    x = GradTensor([1, 2, 3, 4], [2, 2], 1)
-    y = GradTensor([1, 2, 3, 4], [2, 2], 1)
-    truth = GradTensor([7, 10, 15, 22], [2, 2], 1)
+    x = GradTensor([1, 2, 3, 4], [2, 2], 0, 1)
+    y = GradTensor([1, 2, 3, 4], [2, 2], 0, 1)
+    truth = GradTensor([7, 10, 15, 22], [2, 2], 0, 1)
     self.assertEqual(x @ y, truth)
 
 if __name__ == "__main__": 
