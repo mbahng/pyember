5259cf7c4b23559c9b39277766af52277993f480
Muchang Bahng <bahngmc@gmail.com>
2025-01-01 01:23:06 -0500

    debug

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, find_namespace_packages
 from setuptools.command.build_ext import build_ext
 from setuptools import Extension
 import subprocess
@@ -11,6 +11,48 @@ class CMakeExtension(Extension):
         super().__init__(name, sources=[])
 
 class CMakeBuildExt(build_ext):
+
+  def run(self):
+    # Create all necessary directories first
+    ember_dirs = [
+        os.path.join(self.build_lib, 'ember'),
+        os.path.join(self.build_lib, 'ember/aten'),
+        os.path.join(self.build_lib, 'ember/datasets'),
+        os.path.join(self.build_lib, 'ember/models'),
+        os.path.join(self.build_lib, 'ember/models/supervised'),
+        os.path.join(self.build_lib, 'ember/models/unsupervised'),
+        os.path.join(self.build_lib, 'ember/objectives'),
+        os.path.join(self.build_lib, 'ember/optimizers'),
+        os.path.join(self.build_lib, 'ember/samplers')
+    ]
+    
+    for directory in ember_dirs:
+        os.makedirs(directory, exist_ok=True)
+        
+    # Now proceed with normal build
+    super().run()
+    
+    # Copy Python files
+    src_base = 'ember'
+    dest_base = os.path.join(self.build_lib, 'ember')
+    
+    # Helper function to copy files
+    def copy_module_files(src_dir, dest_dir):
+        if not os.path.exists(dest_dir):
+            os.makedirs(dest_dir)
+        for file in glob.glob(os.path.join(src_dir, '*.py')) + glob.glob(os.path.join(src_dir, '*.pyi')):
+            shutil.copy2(file, dest_dir)
+
+    # Copy all module files
+    copy_module_files(src_base, dest_base)
+    copy_module_files(os.path.join(src_base, 'datasets'), os.path.join(dest_base, 'datasets'))
+    copy_module_files(os.path.join(src_base, 'models'), os.path.join(dest_base, 'models'))
+    copy_module_files(os.path.join(src_base, 'models/supervised'), os.path.join(dest_base, 'models/supervised'))
+    copy_module_files(os.path.join(src_base, 'models/unsupervised'), os.path.join(dest_base, 'models/unsupervised'))
+    copy_module_files(os.path.join(src_base, 'objectives'), os.path.join(dest_base, 'objectives'))
+    copy_module_files(os.path.join(src_base, 'optimizers'), os.path.join(dest_base, 'optimizers'))
+    copy_module_files(os.path.join(src_base, 'samplers'), os.path.join(dest_base, 'samplers'))
+
   def build_extension(self, ext):
     # remove all previous build directories and .so files if they exist
     for so_file in glob.glob(os.path.join("ember", "*.so")): 
@@ -59,30 +101,34 @@ class CMakeBuildExt(build_ext):
     shutil.move(built_so, "ember")
     print(f"Successfully moved {built_so} to ember/")
 
-packages = find_packages() 
-packages.append("ember.aten")
-
-ember_package_data = ["*"]
-# [
-#   "ember/*", 
-#   "ember/aten/*",
-#   "ember/datasets/*", 
-#   "ember/models/*", 
-#   "ember/models/supervised/*", 
-#   "ember/models/unsupervised/*", 
-#   "ember/objectives/*", 
-#   "ember/optimizers/*", 
-#   "ember/samplers/*"
-# ]
 package_data = {
-  "ember" : ember_package_data
+    "ember": ["*.so", "*.pyi", "*.py"],
+    "ember.aten": ["*.pyi"],
+    "ember.datasets": ["*.py"],
+    "ember.models": ["*.py", "supervised/*.py", "unsupervised/*.py"],
+    "ember.objectives": ["*.py"],
+    "ember.optimizers": ["*.py"],
+    "ember.samplers": ["*.py"]
 }
 
-setup( 
-  packages = packages,
-  package_data = package_data, 
-  ext_modules = [CMakeExtension('ember.aten')], 
-  cmdclass={'build_ext': CMakeBuildExt},
-  zip_safe = False, 
-  include_package_data = True
+packages = find_packages(include=['ember*'])
+
+setup(
+    packages=packages,
+    package_data=package_data,
+    include_package_data=True,
+    data_files=[
+        ('ember', ['ember/__init__.py']),
+        ('ember/aten', ['ember/aten/__init__.pyi']),
+        ('ember/datasets', glob.glob('ember/datasets/*.py')),
+        ('ember/models', glob.glob('ember/models/*.py')),
+        ('ember/models/supervised', glob.glob('ember/models/supervised/*.py')),
+        ('ember/models/unsupervised', glob.glob('ember/models/unsupervised/*.py')),
+        ('ember/objectives', glob.glob('ember/objectives/*.py')),
+        ('ember/optimizers', glob.glob('ember/optimizers/*.py')),
+        ('ember/samplers', glob.glob('ember/samplers/*.py'))
+    ],
+    ext_modules=[CMakeExtension('ember.aten')],
+    cmdclass={'build_ext': CMakeBuildExt},
+    zip_safe=False
 )
