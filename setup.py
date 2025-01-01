a7e9baad4a074ac944fc525f550f3a936d050ebf
Muchang Bahng <bahngmc@gmail.com>
2024-12-31 21:26:16 -0500

    debug

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ class CMakeBuildExt(build_ext):
     print(f"Successfully moved {built_so} to ember/")
 
 packages = find_packages() 
-packages.extend("ember.aten")
+packages.append("ember.aten")
 print("===================")
 for p in packages: 
   print(p)
